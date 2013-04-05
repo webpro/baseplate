9b0539f26e0ad32fe00d74efd01b217a58298199
Lars Kappert <lars@webpro.nl>
2013-04-05 12:08:42 +0200

    Separated rjs plugins from core config

--- src/core-demo/main.js
+++ src/core-demo/main.js
@@ -13,13 +13,10 @@ require.config({
         'jquery': '../lib/jquery/jquery.min',
         'lodash': '../lib/lodash/lodash.min',
         'backbone': '../lib/backbone/backbone-min',
-        'hogan': '../lib/hogan/web/builds/2.0.0/hogan-2.0.0.min.amd',
 
         // 3rd-party (extensions)
-        'text': '../lib/requirejs-text/text',
-        'backbone.stickit': '../lib/backbone.stickit/backbone.stickit',
+        'backbone.stickit': '../lib/backbone.stickit/backbone.stickit'
 
-        'hgn': '../rjs-plugin/requirejs-hogan/hogan',
     },
     packages: [
         { name: 'when', location: '../lib/when', main: './when' }
---
+++ src/core-demo/plugins.js
@@ -0,0 +1,8 @@
+require.config({
+    deps: ['text', 'hgn'],
+    paths: {
+        'text': '../lib/requirejs-text/text',
+        'hgn': '../rjs-plugin/requirejs-hogan/hogan',
+        'hogan': '../lib/hogan/web/builds/2.0.0/hogan-2.0.0.amd'
+    }
+});
