62426a729dd2de7f42ea43d0d5366bea141230c7
Lars Kappert <lars@webpro.nl>
2013-05-09 13:39:46 +0200

    Getting rid of opinionated crap

--- src/core-demo/plugins.js
+++
@@ -1,8 +0,0 @@
-require.config({
-    deps: ['text', 'hgn'],
-    paths: {
-        'text': '../lib/requirejs-text/text',
-        'hgn': '../rjs-plugin/requirejs-hogan/hogan',
-        'hogan': '../lib/hogan/web/builds/2.0.0/hogan-2.0.0.amd'
-    }
-});
