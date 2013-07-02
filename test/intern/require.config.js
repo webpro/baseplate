174973bdda051959a951c4c4bfca3fc496280e73
Lars Kappert <lars@webpro.nl>
2013-07-03 00:36:14 +0200

    Added test/intern example (browser only)

---
+++ test/intern/require.config.js
@@ -0,0 +1,18 @@
+require(['../../src/app-demo/main'], function() {
+    require.config({
+        baseUrl: '../../src/app-demo',
+        packages: [
+            { name: 'intern', location: '../../node_modules/intern' }
+        ],
+        map: {
+            intern: {
+                dojo: 'intern/node_modules/dojo',
+                chai: 'intern/node_modules/chai/chai'
+            }
+        },
+        paths: {
+            specs: '../../test/intern/specs'
+        },
+        deps: ['intern/client']
+    });
+});
---
+++ test/intern/specs/modelA.spec.js
@@ -0,0 +1,23 @@
+define([
+    'intern!bdd',
+    'intern/chai!expect',
+    'view/moduleA/modelA'
+], function(bdd, expect, ModelA) {
+
+    with (bdd) {
+
+        describe('model/modelA', function() {
+
+            var model = new ModelA();
+
+            it('should have a default value', function() {
+
+                var actual = model.get('valueA');
+                var expected = 1;
+
+                expect(actual).to.equal(expected);
+
+            });
+        });
+    }
+});
