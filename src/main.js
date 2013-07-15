2923f279a19239a7ca4e06288ebbf48f8c771a1d
Lars Kappert <lars@webpro.nl>
2013-07-15 14:46:43 +0200

    relative module id

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-require(['require.config'], function() {
+require(['./require.config'], function() {
     Array.prototype.forEach.call(document.querySelectorAll('[data-moduleId]'), function(element) {
         require([element.getAttribute('data-moduleId')], function(ModuleDefinition) {
             new ModuleDefinition({el: element});
