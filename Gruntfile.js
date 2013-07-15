0bce84cc00c566f708ee4fc1f6b0f3755bd4d814
Lars Kappert <lars@webpro.nl>
2013-07-15 14:46:18 +0200

    uglify2

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
                     dir: 'dist/',
                     skipDirOptimize: true,
                     optimizeCss: 'none',
+                    optimize: 'uglify2',
                     modules: [{
                         name: 'view/moduleA/index',
                         exclude: ['backbone', 'hb']
